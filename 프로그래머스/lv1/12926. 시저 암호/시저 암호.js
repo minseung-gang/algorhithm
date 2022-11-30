3d694a90bbc50e6decd891095219e10b25de3e2a
강민승 <83339863+minseung-gang@users.noreply.github.com>
2022-11-30 10:55:19 +0900

    [level 1] Title: 시저 암호, Time: 38.55 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,18 +1,22 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
 
-    for(var i =0; i <s.length; i++){
-        var text = s[i];
-        if(text == ' ') {
-            answer += ' '; 
-            continue;
-        }
-        var textArr = upper.includes(text) ? upper : lower;
-        var index = textArr.indexOf(text)+n;
-        if(index >= textArr.length) index -= textArr.length;
-        answer += textArr[index];
+    if (s[i] == " ") {
+      answer += " ";
+      continue;
     }
-    return answer;
+    if (s[i] === s[i].toUpperCase()) {
+      let idx = alphabet.indexOf(s[i].toLowerCase());
+      let result_idx = (idx + n) % 26;
+      answer += alphabet[result_idx].toUpperCase();
+    }
+    if (s[i] === s[i].toLowerCase()) {
+      let idx = alphabet.indexOf(s[i].toLowerCase());
+      let result_idx = (idx + n) % 26;
+      answer += alphabet[result_idx].toLowerCase();
+    }
+  }
+  return answer;
 }
