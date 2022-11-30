2b0da8443639b2a2640841e330c57e6b457420d8
강민승 <83339863+minseung-gang@users.noreply.github.com>
2022-11-30 10:25:20 +0900

    [level 1] Title: 시저 암호, Time: 4.34 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -2,26 +2,40 @@
 // 소문자와 대문자 공백을 사용하여 패턴을 맞추어야한다.
 // 
 
-function solution(s, n) {
-  s.split(" ");
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let answer = "";
-  for (let i = 0; i < s.length; i++) {
 
-    if (s[i] == " ") {
-      answer += " ";
-      continue;
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
     }
-    if (s[i] === s[i].toUpperCase()) {
-      let item_idx = alphabet.indexOf(s[i].toLowerCase());
-      let idx = (item_idx + n) % 26;
-      answer += alphabet[idx].toUpperCase();
-    }
-    if (s[i] === s[i].toLowerCase()) {
-      let item_idx = alphabet.indexOf(s[i].toLowerCase());
-      let idx = (item_idx + n) % 26;
-      answer += alphabet[idx].toLowerCase();
-    }
-  }
-  return answer;
+    return answer;
 }
